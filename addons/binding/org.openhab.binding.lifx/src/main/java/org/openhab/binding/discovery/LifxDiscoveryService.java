b5b9c02fc86f204ace1298f7349ddb900c0ed983
fa2k <pmb@fa2k.net>
2016-04-09 16:50:35 +0200

    Started work on discovery but something else came up

--- addons/binding/org.openhab.binding.lifx/src/main/java/org/openhab/binding/discovery/LifxDiscoveryService.java
+++ addons/binding/org.openhab.binding.lifx/src/main/java/org/openhab/binding/discovery/LifxDiscoveryService.java
@@ -1,18 +1,15 @@
 package org.openhab.binding.discovery;
 
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LifxDiscoveryService extends AbstractDiscoveryService implements Runnable {
+public class LifxDiscoveryService extends AbstractDiscoveryService {
 
     private final Logger logger = LoggerFactory.getLogger(LifxDiscoveryService.class);
     private static final int PROBE_INTERVAL = 60;
 
-    ScheduledFuture<?> discoveryJob;
+    private boolean scanActive = false, backgroundDiscoveryActive = false;
 
     public LifxDiscoveryService(int timeout) throws IllegalArgumentException {
         super(timeout);
@@ -20,39 +17,24 @@ public class LifxDiscoveryService extends AbstractDiscoveryService implements Ru
 
     @Override
     protected void startScan() {
+
     }
 
     @Override
     protected synchronized void stopScan() {
         super.stopScan();
+        scanActive = false;
         removeOlderResults(getTimestampOfLastScan());
     }
 
     @Override
     protected void startBackgroundDiscovery() {
-        logger.debug("Start LIFX device background discovery");
-        if (discoveryJob == null || discoveryJob.isCancelled()) {
-            discoveryJob = scheduler.scheduleAtFixedRate(this, 0, PROBE_INTERVAL, TimeUnit.SECONDS);
-        }
+        backgroundDiscoveryActive = true;
     }
 
     @Override
     protected void stopBackgroundDiscovery() {
-        logger.debug("Stop LIFX device background discovery");
-        if (discoveryJob != null && !discoveryJob.isCancelled()) {
-            discoveryJob.cancel(true);
-            discoveryJob = null;
-        }
-    }
-
-    @Override
-    public void run() {
-        // Discovery task: send a broadcast
-
-    }
-
-    public void sendBroadcast() {
-
+        backgroundDiscoveryActive = false;
     }
 
 }
