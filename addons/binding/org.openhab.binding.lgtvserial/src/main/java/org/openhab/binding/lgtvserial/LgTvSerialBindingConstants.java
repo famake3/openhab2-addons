11b350b9886281c8039740542bf29827d6f2f4f1
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2016-05-16 07:02:33 +0200

    LG TV serial control, initial binding skeleton

---
+++ addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/LgTvSerialBindingConstants.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.lgtvserial;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link LgTvSerialBinding} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Famake - Initial contribution
+ */
+public class LgTvSerialBindingConstants {
+
+    public static final String BINDING_ID = "lgtvserial";
+
+    // List of all Thing Type UIDs
+    public final static ThingTypeUID THING_TYPE_LGTV = new ThingTypeUID(BINDING_ID, "lgtv");
+
+    // List of all Channel ids
+    public final static String CHANNEL_POWER = "power";
+
+}
