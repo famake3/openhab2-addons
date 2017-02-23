a62deae22a5b6e3fc5a2dafc1282e74f0cce31c3
famake <mikaelaasen@hotmail.com>
2017-02-23 21:14:56 +0100

    Changed to use NRSerialPort

--- addons/binding/org.openhab.binding.benqprojector/src/main/java/org/openhab/binding/benqprojector/handler/BenqProjectorHandler.java
+++ addons/binding/org.openhab.binding.benqprojector/src/main/java/org/openhab/binding/benqprojector/handler/BenqProjectorHandler.java
@@ -9,6 +9,9 @@ package org.openhab.binding.benqprojector.handler;
 
 import static org.openhab.binding.benqprojector.BenqProjectorBindingConstants.POWER;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
@@ -22,8 +25,7 @@ import org.eclipse.smarthome.core.types.RefreshType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jssc.SerialPort;
-import jssc.SerialPortException;
+import gnu.io.NRSerialPort;
 
 /**
  * The {@link BenqProjectorHandler} is responsible for handling commands, which are
@@ -33,11 +35,11 @@ import jssc.SerialPortException;
  */
 public class BenqProjectorHandler extends BaseThingHandler implements Runnable {
 
-    private static final int TIMEOUT_MS = 50;
-    private static final int N_TRIES = 100, N_TRIES_FAST = 2;
     private Logger logger = LoggerFactory.getLogger(BenqProjectorHandler.class);
-    private SerialPort port;
+    private NRSerialPort serialPort;
     private boolean on = false;
+    private OutputStreamWriter output;
+    private InputStream input;
 
     public BenqProjectorHandler(Thing thing) {
         super(thing);
@@ -49,7 +51,7 @@ public class BenqProjectorHandler extends BaseThingHandler implements Runnable {
         updateStatus(ThingStatus.ONLINE);
         try {
             updateState(channelUID, isOn() ? OnOffType.ON : OnOffType.OFF);
-        } catch (SerialPortException | CommunicationException e) {
+        } catch (CommunicationException | IOException e) {
             updateStatus(ThingStatus.OFFLINE);
         }
         BigDecimal pollingInterval = (BigDecimal) getThing().getConfiguration().get("polling-interval");
@@ -61,112 +63,117 @@ public class BenqProjectorHandler extends BaseThingHandler implements Runnable {
 
     @Override
     public void dispose() {
-        try {
-            port.closePort();
-        } catch (SerialPortException e) {
-        }
+        serialPort.disconnect();
     }
 
-    private boolean isOn() throws SerialPortException, CommunicationException {
-        SerialPort port = getSerialPort();
-        port.readBytes();
-        port.writeBytes(("X000X").getBytes());
+    private boolean isOn() throws CommunicationException, IOException {
+        connect();
+        output.write("X000X");
+        output.flush();
         String expected = "X000XRS232_OKX0_0X";
-        int waiting = 0;
-        while (port.getInputBufferBytesCount() < expected.length() && waiting < N_TRIES_FAST) {
-            try {
-                Thread.sleep(TIMEOUT_MS);
-            } catch (InterruptedException e) {
+        int numRead = 0;
+        int length = expected.length();
+        char[] reply = new char[length];
+        while (numRead < expected.length()) {
+            int b = input.read();
+            if (b == -1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return false;
+                }
+            } else {
+                reply[numRead++] = (char) b;
             }
-            ++waiting;
         }
-        if (waiting == N_TRIES_FAST) {
-            on = false;
-        } else {
-            readReply(expected, port);
+        if (new String(reply).equals(expected)) {
             on = true;
+            updateStatus(ThingStatus.ONLINE);
+        } else {
+            on = false;
+            throw new CommunicationException("Unexpected reply from projector");
         }
         return on;
     }
 
-    public void on() throws SerialPortException, CommunicationException {
+    public void on() throws CommunicationException, IOException {
         command("X001X", "X0_1X");
     }
 
-    public void off() throws SerialPortException, CommunicationException {
+    public void off() throws CommunicationException, IOException {
         command("X002X", "X002XX0_2X");
     }
 
-    private void command(String cmd, String expectedReply) throws SerialPortException, CommunicationException {
-        SerialPort port = getSerialPort();
-        port.readBytes();
+    private void command(String cmd, String expectedReply) throws CommunicationException, IOException {
+        connect();
 
         // System.out.println("Sending command");
-        if (port.writeBytes(cmd.getBytes())) {
-            int waiting = 0;
-            while (port.getInputBufferBytesCount() < expectedReply.length() && waiting < N_TRIES) {
-                try {
-                    Thread.sleep(TIMEOUT_MS);
-                } catch (InterruptedException e) {
+        output.write(cmd);
+        output.flush();
+        int numRead = 0;
+        int length = expectedReply.length();
+        char[] reply = new char[length];
+        int numTries = 5;
+        while (numRead < expectedReply.length()) {
+            int b = 0;
+            try {
+                b = input.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+                if (numTries-- == 0) {
+                    throw new CommunicationException("Input error", e);
                 }
-                ++waiting;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    return;
+                }
+                continue;
             }
-            if (waiting == N_TRIES) {
-                throw new CommunicationException("Serial communication failed");
+            if (b == -1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            } else {
+                reply[numRead++] = (char) b;
             }
-            // System.out.println("Sent bytes to projector");
-            readReply(expectedReply, port);
-            // System.out.println("Read reply");
+        }
+        if (new String(reply).equals(expectedReply)) {
+            updateStatus(ThingStatus.ONLINE);
         } else {
-            logger.error("Could not send to projector");
-            throw new CommunicationException("Could not send to projector");
+            throw new CommunicationException("Unexpected reply from projector");
         }
     }
 
-    private void readReply(String expectedReply, SerialPort port) throws SerialPortException, CommunicationException {
-        for (char c : expectedReply.toCharArray()) {
-            byte[] in = port.readBytes(1);
-            if (in == null || in.length == 0) {
-                throw new CommunicationException("Invalid response from projector");
-            } else if (in[0] != c) {
-                throw new CommunicationException("Invalid response from projector");
-            }
-        }
-        updateStatus(ThingStatus.ONLINE);
-    }
-
-    private SerialPort getSerialPort() throws SerialPortException {
-        if (port == null || !port.isOpened()) {
+    private synchronized void connect() {
+        if (serialPort == null || !serialPort.isConnected()) {
             String comPort = (String) getThing().getConfiguration().get("port");
-            port = new SerialPort(comPort);
-            port.openPort();
-            port.setParams(112500, 8, 1, 0);
+            serialPort = new NRSerialPort(comPort, 112500);
+            serialPort.connect();
+            output = new OutputStreamWriter(serialPort.getOutputStream());
+            input = serialPort.getInputStream();
             updateStatus(ThingStatus.ONLINE);
         }
-        return port;
     }
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-
         try {
             if (channelUID.getId().equals(POWER)) {
                 handlePower(channelUID, command);
             }
-        } catch (SerialPortException | CommunicationException e) {
-            try {
-                port.closePort();
-            } catch (SerialPortException e1) {
-            }
-            port = null;
+        } catch (CommunicationException | IOException e) {
+            serialPort.disconnect();
+            serialPort = null;
             updateStatus(ThingStatus.OFFLINE);
             logger.error("Serial port error: " + e.getMessage());
             on = false;
         }
     }
 
-    private void handlePower(ChannelUID channelUID, Command command)
-            throws SerialPortException, CommunicationException {
+    private void handlePower(ChannelUID channelUID, Command command) throws CommunicationException, IOException {
         if (command != null) {
             if (command instanceof OnOffType) {
                 if (command.equals(OnOffType.ON)) {
@@ -188,7 +195,7 @@ public class BenqProjectorHandler extends BaseThingHandler implements Runnable {
             boolean powerOn = isOn();
             updateStatus(ThingStatus.ONLINE);
             updateState(channelUID, powerOn ? OnOffType.ON : OnOffType.OFF);
-        } catch (SerialPortException | CommunicationException e) {
+        } catch (CommunicationException | IOException e) {
             updateStatus(ThingStatus.OFFLINE);
         }
     }
