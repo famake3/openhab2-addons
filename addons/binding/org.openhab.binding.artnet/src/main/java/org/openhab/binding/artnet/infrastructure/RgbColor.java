609bdaabb74e0ca45766cb0e24694c2f9c01e2b2
fa2k <pmb@fa2k.net>
2017-08-10 21:50:27 +0200

    This file was missing from master (still incomplete work)

---
+++ addons/binding/org.openhab.binding.artnet/src/main/java/org/openhab/binding/artnet/infrastructure/RgbColor.java
@@ -0,0 +1,13 @@
+package org.openhab.binding.artnet.infrastructure;
+
+public class RgbColor {
+
+    public final float r, g, b;
+
+    public RgbColor(float r, float g, float b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+}
