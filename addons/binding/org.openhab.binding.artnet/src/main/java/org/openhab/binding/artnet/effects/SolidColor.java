bf4a211156e1be31fe9234bd94c1c0df8f73f8a9
fa2k <pmb@fa2k.net>
2019-01-26 14:22:08 +0100

    initial development wrok

--- addons/binding/org.openhab.binding.artnet/src/main/java/org/openhab/binding/artnet/effects/SolidColor.java
+++ addons/binding/org.openhab.binding.artnet/src/main/java/org/openhab/binding/artnet/effects/SolidColor.java
@@ -7,6 +7,7 @@ import org.openhab.binding.artnet.infrastructure.Model;
 public class SolidColor extends Layer {
 
     private boolean generated = false;
+    private Color color;
 
     public enum Mode {
         SPACE_DITHER
@@ -14,31 +15,26 @@ public class SolidColor extends Layer {
 
     public SolidColor(Model model, Color color) {
         super(model);
-        generate(color);
+        this.color = color;
     }
 
     @Override
-    public Layer update(double timeCode) {
+    public Layer update(long timeCode) {
         if (!generated) {
+            generate();
         }
-        return null;
+        return this;
     }
 
-    private void generate(Color color) {
+    private void generate() {
 
         int ir = (int) color.r, ig = (int) color.g, ib = (int) color.b;
         double rx = color.r - ir, gx = color.g - ig, bx = color.b - ib;
 
-        /*
-         * for (List<byte[]> bufferList : buffers) {
-         * for (byte[] data : bufferList) {
-         * for (int i = 0; i < data.length; ++i) {
-         * data[i * 3] = (byte) (ir + (Math.random() < rx ? 1 : 0));
-         * data[i * 3 + 1] = (byte) (ig + (Math.random() < gx ? 1 : 0));
-         * data[i * 3 + 2] = (byte) (ib + (Math.random() < bx ? 1 : 0));
-         * }
-         * }
-         * }
-         */
+        for (int i = 0; i < data.length; ++i) {
+            data[i * 3] = (byte) (ir + (Math.random() < rx ? 1 : 0));
+            data[i * 3 + 1] = (byte) (ig + (Math.random() < gx ? 1 : 0));
+            data[i * 3 + 2] = (byte) (ib + (Math.random() < bx ? 1 : 0));
+        }
     }
 }
--- addons/binding/org.openhab.binding.artnet/src/main/java/org/openhab/binding/artnet/effects/StableFader.java
+++ addons/binding/org.openhab.binding.artnet/src/main/java/org/openhab/binding/artnet/effects/StableFader.java
@@ -5,14 +5,37 @@ import org.openhab.binding.artnet.infrastructure.Model;
 
 public class StableFader extends Layer {
 
-    public StableFader(Model model, Layer from, Layer to) {
+    private long startTime, duration;
+    private double[] roundAffinity;
+    private Layer from, to;
+
+    public StableFader(Model model, Layer from, Layer to, long startTime, long duration) {
         super(model);
+        this.startTime = startTime;
+        this.duration = duration;
+        this.from = from;
+        this.to = to;
+        if (from.data.length != model.getSize() || to.data.length != model.getSize()) {
+            throw new IllegalArgumentException(
+                    "From and To layers have different sizes, " + from.data.length + " and " + to.data.length + ".");
+        }
+        for (int i = 0; i < model.getSize(); ++i) {
+            roundAffinity[i] = Math.random();
+        }
     }
 
     @Override
     public Layer update(long timeCode) {
-        // TODO Auto-generated method stub
-        return null;
+        double fraction = (timeCode - startTime) / (double) duration;
+        if (fraction >= 1.0) {
+            return to.update(timeCode);
+        }
+        from = from.update(timeCode);
+        to = to.update(timeCode);
+        for (int i = 0; i < model.getSize(); ++i) {
+            data[i] = (byte) (from.data[i] * (1 - fraction) + to.data[i] * fraction + roundAffinity[i]);
+        }
+        return this;
     }
 
 }
